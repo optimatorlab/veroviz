9ef1b41edf5dca6c734747f11d66b0e7f77f2ec4
Isaac0821 <isaac0821@sina.com>
2019-10-15 18:33:31 -0400

    \xe2

--- docs/conf.py
+++ docs/conf.py
@@ -22,9 +22,9 @@ copyright = '2019, optimatorlab'
 author = 'Chase Murray; Lan Peng'
 
 # The short X.Y version
-version = '0.2.0'
+version = '0.2.1'
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.1'
 
 
 # -- General configuration ---------------------------------------------------
