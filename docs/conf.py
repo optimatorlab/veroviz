4d83ad4af5da57eec82c721e9313a2358cc26535
isaac0821 <isaac0821@sina.com>
2021-02-17 14:56:23 -0500

    update documentation to 0.4.4

--- docs/conf.py
+++ docs/conf.py
@@ -22,9 +22,9 @@ copyright = '2019, optimatorlab'
 author = 'Chase Murray; Lan Peng'
 
 # The short X.Y version
-version = '0.4.3'
+version = '0.4.4'
 # The full version, including alpha/beta/rc tags
-release = '0.4.3'
+release = '0.4.4'
 
 
 # -- General configuration ---------------------------------------------------
